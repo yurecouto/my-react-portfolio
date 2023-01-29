e617dcc0fc3769908156752d430164e2334977b8
Yure Barbosa Couto <49762865+yurecouto@users.noreply.github.com>
2023-01-29 14:15:27 -0300

    final code fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_URL_LOCALHOST,
+    baseURL: process.env.REACT_APP_URL_RENDER,
 });
 
 export default api;
